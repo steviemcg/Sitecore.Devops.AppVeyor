d1f864a0247ceec0942a38d852775cafe5a3a5fe
Steve McGill <smg@sitecore.net>
2017-03-29 20:04:32 +0100

    trying to get the deploy script working!

--- buildscript/deployment-scripts/TDS/deploy.ps1
+++ buildscript/deployment-scripts/TDS/deploy.ps1
@@ -1,28 +1,32 @@
 $ErrorActionPreference = "Stop"
-$url = $Env:url
+$url = $Env:Url
 $ship_url = "${url}/sitecore_ship"
 
 Function DeployTds {
     Param (
-        [string]$name
+        [string]$name,
+		[string]$ship_url
     )
 
     Write-Host "Deploying TDS package: $name" -ForegroundColor Green
     Invoke-WebRequest -Method POST -Body "path=${pwd}\${name}" -Uri "${ship_url}/package/install" -ContentType application/x-www-form-urlencoded -TimeoutSec 600 -UseBasicParsing
-	Warmup
-
+	
     if($LastExitCode -ne 0) {
       $host.SetShouldExit($LastExitCode)
     }
 }
 
-Function Warmup() {
+Function Warmup {
+	Param (
+		[string]$url
+    )
+	
 	Write-Host "Warming up..." -ForegroundColor Yellow
-	$page = Invoke-WebRequest -Uri ${ship_url}/about -TimeoutSec 600 -UseBasicParsing
+	$page = Invoke-WebRequest -Uri $url -TimeoutSec 600 -UseBasicParsing
 }
 
-Warmup
-DeployTds -name "Devops.AppVeyor.Update"
+Warmup "${ship_url}/about"
+DeployTds "Devops.AppVeyor.Update" $ship_url
 
 Write-Host "Testing site..." -ForegroundColor Yellow
-$page = Invoke-WebRequest -Uri ${url} -TimeoutSec 600 -UseBasicParsing
+Warmup $url
